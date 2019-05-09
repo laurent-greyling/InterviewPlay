fe792b3579c740c822fc7fd05b3bdced5aa84ca8
laurent.greyling@gmail.com <laurent.greyling@gmail.com>
2019-05-09 10:27:01 +0200

    fix tests

--- InterviewPlayTests/InterviewBuildTests.cs
+++ InterviewPlayTests/InterviewBuildTests.cs
@@ -10,13 +10,17 @@ namespace Tests
     public class InterviewBuildTests
     {
         private Mock<IBuildInterview> _mockInterviewBuilder;
+        private Mock<ISqlClient> _mockSqlClient;
         private BuildInterview _target;
 
         [SetUp]
         public void Setup()
         {
             _mockInterviewBuilder = new Mock<IBuildInterview>();
-            _mockInterviewBuilder.Setup(c => c.Build("en")).Returns(It.IsAny<SurveyModel>());
+            _mockSqlClient = new Mock<ISqlClient>();
+            _mockInterviewBuilder.Setup(c => c.Build("en", "respondentId")).Returns(It.IsAny<SurveyModel>());
+            _mockSqlClient.Setup(s => s.CreateRespondentTableIfNotExist(123));
+            _mockSqlClient.Setup(s => s.InsertRespodentDetails(123, "respondentId"));
 
             _target = new BuildInterview(File.ReadAllText("../../../questionnaire.json"));
         }
@@ -24,7 +28,7 @@ namespace Tests
         [Test]
         public void DeserialiseAndBuildModelTest()
         {
-            var model = _target.Build("en");
+            var model = _target.Build("en", "respondentId");
 
             Assert.IsNotNull(model);
             Assert.AreEqual(1000, model.QuestionnaireId);          
@@ -33,7 +37,7 @@ namespace Tests
         [Test]
         public void EnglishSelectedModelReturnsEnglishTest()
         {
-            var model = _target.Build("en");
+            var model = _target.Build("en", "respondentId");
             var enText = model.QuestionnaireItems.Any(c => c.SubjectText == "My work");
 
             Assert.IsTrue(enText);
@@ -42,7 +46,7 @@ namespace Tests
         [Test]
         public void DutchSelectedModelReturnsDutchTest()
         {
-            var model = _target.Build("nl");
+            var model = _target.Build("nl", "respondentId");
             var enText = model.QuestionnaireItems.Any(c => c.SubjectText == "Mijn werk");
 
             Assert.IsTrue(enText);
